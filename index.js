e2d1c869e42d2abbc80440a76e286120b98a6d49
Developer-Rathod <developer.rathod@gmail.com>
2019-12-11 12:12:02 +0200

    3.1 done

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ const PORT = 3001;
 app.listen(PORT,()=>{
     console.log(`Backend server start on port ${PORT}`);
 });
-let persons =[
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+      },
+      {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+      },
+      {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+      },
+      {
+        "name": "kalpana",
+        "number": "23456",
+        "id": 5
+      }
     
 ]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+  })
