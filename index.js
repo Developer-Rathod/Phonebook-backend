201a6619a898b12263c9a99735693f82d6bb97e1
Developer-Rathod <developer.rathod@gmail.com>
2019-12-11 12:04:54 +0200

    Start backend app

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = 3001;
+app.listen(PORT,()=>{
+    console.log(`Backend server start on port ${PORT}`);
+});
+let persons =[
+    
+]
